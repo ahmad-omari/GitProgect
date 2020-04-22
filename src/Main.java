f24ad65a1fb1ae83e54fa230c955cb81629a9f7c
user1 <user1@example.com>
2020-04-22 04:27:42 +0300

    Modyfing classes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Min between 2 and 9 is "+MinMax.getMin(2,9));
+        System.out.println("Max between 2 and 9 is "+MinMax.getMax(2,9));
     }
 }
--- src/MinMax.java
+++ src/MinMax.java
@@ -2,4 +2,7 @@ public class MinMax {
     public static int getMin(int x,int y){
         return Math.min(x,y);
     }
+    public static int getMax(int x,int y){
+        return Math.max(x,y);
+    }
 }
