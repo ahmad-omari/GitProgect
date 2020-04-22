aff64fd988631eb8093a97f974c979a08635111e
Ahmad Omari <alomari.ah98@gmail.com>
2020-04-22 04:21:24 +0300

    Adding src file

---
+++ src/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Min between 2 and 9 is "+MinMax.getMin(2,9));
+    }
+}
---
+++ src/MinMax.java
@@ -0,0 +1,5 @@
+public class MinMax {
+    public static int getMin(int x,int y){
+        return Math.min(x,y);
+    }
+}
